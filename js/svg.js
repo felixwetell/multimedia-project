ab1945907c51c217296e4719b1957a33333fac1b
Felix Wetell <felixwetell@users.noreply.github.com>
2018-05-31 10:30:08 +0200

    Added SVG logos, SVG diagram with animation and some more stuff

--- js/svg.js
+++ js/svg.js
@@ -3,6 +3,7 @@
 window.onload = function ()
 {
    animationHandler();
+   listener();
 };
 
 function animationHandler()
@@ -27,4 +28,55 @@ function animationHandler()
             scale.classList.remove ( 'animation-scale' );
         }
     );
+}
+
+function listener ()
+{
+    let button = document.querySelector ( '#diagramButton' );
+
+    button.addEventListener ( 'click', animateDiagram, false );
+
+    function animateDiagram ()
+    {
+        let red, blue, grey;
+
+        red = document.querySelector ( '#red' );
+        blue = document.querySelector ( '#blue' );
+        grey = document.querySelector ( '#grey' );
+
+        red.setAttribute ( 'height', '200' );
+        blue.setAttribute ( 'height', '250' );
+        grey.setAttribute ( 'height', '150' );
+
+        red.animate({
+            transform: ['scaleX(0)', 'none'],
+        }, {
+            height: 200,
+            duration: 1000,
+            fill: 'forwards',
+            easing: 'ease',
+        });
+
+        blue.animate({
+            transform: ['scaleX(0)', 'none'],
+        }, {
+            height: 200,
+            duration: 1000,
+            fill: 'forwards',
+            easing: 'ease',
+        });
+
+        grey.animate({
+            transform: ['scaleX(0)', 'none'],
+        }, {
+            height: 200,
+            duration: 1000,
+            fill: 'forwards',
+            easing: 'ease',
+        });
+
+        // red = 200
+        // blue = 250
+        // grey = 150
+    }
 }
